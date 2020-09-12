c2802659a0a9f5317a8fa4e3b25f3ba2eaa969a6
Devang Kamble <risingentropy20@gmail.com>
2020-09-12 22:57:56 +0530

    Configuring Models

---
+++ david/scorer/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+
+# Create your models here.
+
+class Teacher(models.Model):
+    id = models.Field(primary_key = True)
+    mail = models.EmailField
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    department = models.CharField(max_length=50)
+    designation = models.CharField(max_length=50)
+    subject = models.CharField(max_length=50)
+
+class Student(models.Model):
+    id = models.Field(primary_key = True)
+    mail = models.EmailField
+    fName = models.CharField(max_length=50)
+    lName = models.CharField(max_length=50)
+    year = models.PositiveIntegerField
+    branch = models.CharField(max_length=50)
+
+class DiscreteMath(models.Model):
+    id = models.Field(primary_key = True)
+    rollNo = models.PositiveIntegerField
+    grade = models.CharField(max_length=2)
+
+class DataCommunication(models.Model):
+    id = models.Field(primary_key = True)
+    rollNo = models.PositiveIntegerField
+    grade = models.CharField(max_length=2)
+
+class ComputerOrganisationArchitecture(models.Model):
+    id = models.Field(primary_key = True)
+    rollNo = models.PositiveIntegerField
+    grade = models.CharField(max_length=2)
+
+class SoftwareEngineering(models.Model):
+    id = models.Field(primary_key = True)
+    rollNo = models.PositiveIntegerField
+    grade = models.CharField(max_length=2)
+
+class ProbabilityAndStatistics(models.Model):
+    id = models.Field(primary_key = True)
+    rollNo = models.PositiveIntegerField
+    grade = models.CharField(max_length=2)
+
