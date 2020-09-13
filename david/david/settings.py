44690fa5ddd51b1c7dcac41d6363c9aaf689d45a
KshitijSabale <kshitijsabale14@gmail.com>
2020-09-13 12:10:58 +0530

    landing page

--- david/david/settings.py
+++ david/david/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,10 +56,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'david.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,'Webpages')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
