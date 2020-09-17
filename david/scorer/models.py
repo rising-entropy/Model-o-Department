c1f5731e5b4e7f8af7f59fee8fb27ee29f29cd28
Devang Kamble <risingentropy20@gmail.com>
2020-09-17 09:28:29 +0530

    Optimised Model

--- david/scorer/models.py
+++ david/scorer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Teacher(models.Model):
-    id = models.Field(primary_key = True)
+    #id = models.Field(primary_key = True)
     mail = models.EmailField
     fName = models.CharField(max_length=50)
     lName = models.CharField(max_length=50)
@@ -12,40 +12,16 @@ class Teacher(models.Model):
     subject = models.CharField(max_length=50)
 
 class Student(models.Model):
-    id = models.Field(primary_key = True)
+    #id = models.Field(primary_key = True)
     mail = models.EmailField
     fName = models.CharField(max_length=50)
     lName = models.CharField(max_length=50)
     year = models.PositiveIntegerField
     branch = models.CharField(max_length=50)
     rollNo = models.PositiveIntegerField
-
-class DiscreteMath(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
-
-class DataCommunication(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
-
-class ComputerOrganisationArchitecture(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
-
-class SoftwareEngineering(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
-
-class ProbabilityAndStatistics(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
-
-class DataStructures(models.Model):
-    id = models.Field(primary_key = True)
-    rollNo = models.PositiveIntegerField
-    grade = models.CharField(max_length=2)
+    DMgrade = models.CharField(max_length=2, default="NA")
+    DCgrade = models.CharField(max_length=2, default="NA")
+    COAgrade = models.CharField(max_length=2, default="NA")
+    SEgrade = models.CharField(max_length=2, default="NA")
+    PaSgrade = models.CharField(max_length=2, default="NA")
+    DSgrade = models.CharField(max_length=2, default="NA")
