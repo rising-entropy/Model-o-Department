7f405d6b4e6e0fb469e8fd897a297892283ee201
Devang Kamble <risingentropy20@gmail.com>
2020-09-17 13:22:48 +0530

    Integrate Pages

--- david/scorer/urls.py
+++ david/scorer/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     path('',views.landing_page,name='login_page'),
-    path('student/',views.student_page,name='student_page'),
+    path('student/<int:year>/<int:rollNo>',views.student_page,name='student_page'),
+    path('teacher/',views.student_page,name='teacher_page'),
+    path('teacher/create',views.student_page,name='teacher_create'),
+    path('teacher/list',views.student_page,name='teacher_list'),
+    path('teacher/detail/<int:year>/<int:rollNo>',views.student_page,name='teacher_detail')
 ]
 
 
--- david/scorer/views.py
+++ david/scorer/views.py
@@ -3,5 +3,17 @@ from django.shortcuts import render
 def landing_page(request):
     return render(request,'login.html')
 
-def student_page(request):
+def student_page(request, year, rollNo):
     return render(request,'student1.html')
+
+def teacher_main(request):
+    return render(request, 'Teacher1.html')
+
+def teacher_create(request):
+    return render(request, 'CreateTeach.html')
+
+def teacher_list(request):
+    return render(request, 'ListTeach.html')
+
+def teacher_detail(request, year, rollNo):
+    return render(request, 'DetailTeach.html')
